c17c6aeadf44cbccac0bfa9c4007b9548a868d3d
Rafał Cieślak <ravicious@gmail.com>
2016-03-18 16:20:11 +0100

    Memoize within-bounds? and get-neighbor-coordinates
    These functions are called quite often. Memoizing their results
    should make them faster.
    
    Also, make a bunch of methods from cljs-sweeper.game.board private.

--- src/cljs_sweeper/game/board.cljs
+++ src/cljs_sweeper/game/board.cljs
@@ -8,18 +8,20 @@
 
 (def ^:private min-coordinate 0)
 
-(defn- within-bounds? [width height {:keys [x y]}]
-  (and
-    (<= min-coordinate x (dec width))
-    (<= min-coordinate y (dec height))))
+(def ^:private within-bounds?
+  (memoize
+    (fn [width height {:keys [x y]}]
+      (and
+        (<= min-coordinate x (dec width))
+        (<= min-coordinate y (dec height))))))
 
-(defn calculate-index
+(defn- calculate-index
   "Translates 2D coordinates to an index of 1D array. Returns nil if given coordinates are out of bounds."
   [{width :columns height :rows} {:keys [x y] :as coordinates}]
   (when (within-bounds? width height coordinates)
     (+ x (* width y))))
 
-(defn calculate-coordinates
+(defn- calculate-coordinates
   "Translates 1D array index to 2D coordinates. Returns nil if given index i is out of bounds."
   [{width :columns height :rows} i]
   (let [coordinates {:x (mod i width)
@@ -27,13 +29,14 @@
     (when (within-bounds? width height coordinates)
       coordinates)))
 
-(defn- get-neighbor-coordinates [{:keys [x y]}]
-  [
-   {:x (dec x) :y (dec y)} {:x x :y (dec y)} {:x (inc x) :y (dec y)}
-   {:x (dec x) :y y      }                   {:x (inc x) :y y      }
-   {:x (dec x) :y (inc y)} {:x x :y (inc y)} {:x (inc x) :y (inc y)}
-   ]
-  )
+(def ^:private get-neighbor-coordinates
+  (memoize
+    (fn [{:keys [x y]}]
+      [
+       {:x (dec x) :y (dec y)} {:x x :y (dec y)} {:x (inc x) :y (dec y)}
+       {:x (dec x) :y y      }                   {:x (inc x) :y y      }
+       {:x (dec x) :y (inc y)} {:x x :y (inc y)} {:x (inc x) :y (inc y)}
+       ])))
 
 (defn get-neighbor-indexes-of-index
   ([board-bounds index]
