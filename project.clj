fadc9d3510ea04529a9ae8f96d94ce16f9a2c26e
Rafał Cieślak <ravicious@gmail.com>
2016-03-23 19:24:06 +0100

    Update Clojure, Clojurescript and figwheel

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.34"]
                  [org.clojure/core.async "0.2.374"]
                  [reagent "0.5.1"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-figwheel "0.5.0-3"]]
+            [lein-figwheel "0.5.1"]]
 
   :source-paths ["src"]
 
