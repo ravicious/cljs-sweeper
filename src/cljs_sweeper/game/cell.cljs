0a0940f39e990aa3d3b1a4846504ce3be28d4ad6
Rafał Cieślak <ravicious@gmail.com>
2016-05-03 12:09:07 +0200

    Fix the logic behind attacking an enemy
    * The number of HP decremented is now calculated instead of being
      equal to the enemy power.
    * The exp for killing the enemy is always awarded, even if the enemy
      is more powerful than the player.

--- src/cljs_sweeper/game/cell.cljs
+++ src/cljs_sweeper/game/cell.cljs
@@ -5,7 +5,12 @@
    :visible false
    :surrounding-power nil})
 
-(defn exp-rewarded [power]
+(defn exp-rewarded [{:keys [power]}]
   (if (> power 2)
     (. js/Math (pow 2 (- power 1)))
     power))
+
+(defn health-decremented [{:keys [power]}]
+  (if (> power 1)
+    (* power (dec power))
+    1))
